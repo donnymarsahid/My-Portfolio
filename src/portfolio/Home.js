83c8f60e77791554d246976a3c031ac4951cdb6a
unknown <donnymrshd.94@gmail.com>
2021-07-15 16:01:31 +0700

    Updates

--- src/portfolio/Home.js
+++ src/portfolio/Home.js
@@ -46,112 +46,131 @@ const Home = () => {
             </div>
           </div>
         </header>
-        <div className="information d-flex">
-          <div className="container text-center">
-            <div className="box">
-              <div className="logo-information">
-                <img src={information} alt="information" />
+        <div class="container-custom">
+          <div className="information d-flex">
+            <div className="container text-center">
+              <div className="box">
+                <div className="logo-information">
+                  <img src={information} alt="information" />
+                </div>
+                <p className="pt-4 pb-3">
+                  Saya akan memperkenalkan beberapa{' '}
+                  <Link to="/" className="p">
+                    Portfolio
+                  </Link>{' '}
+                  yang saya buat, dari hasil kerja yang saya pelajari mengenai <strong>Web Design</strong> Maupun <strong>Web Develop</strong> , jika ingin bertanya{' '}
+                  <Link to="/" className="p">
+                    Kontak
+                  </Link>{' '}
+                  saya ya.. Selamat membaca :)
+                </p>
               </div>
-              <p className="pt-4 pb-3">
-                Saya akan memperkenalkan beberapa{' '}
-                <Link to="/" className="p">
-                  Portfolio
-                </Link>{' '}
-                yang saya buat, dari hasil kerja yang saya pelajari mengenai <strong>Web Design</strong> Maupun <strong>Web Develop</strong> , jika ingin bertanya{' '}
-                <Link to="/" className="p">
-                  Kontak
-                </Link>{' '}
-                saya ya.. Selamat membaca :)
-              </p>
             </div>
           </div>
         </div>
-        <div className="content mb-5">
-          <div className="container">
-            <div className="box-text mb-4">
-              <h3>Portfolio</h3>
-            </div>
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="box-portfolio">
-                  <Link to="/">
-                    <img src={landingPage} className="mb-3" alt="landing-page" />
-                  </Link>
-                  <Link to="/" className="h4">
-                    <h4>Landing Page</h4>
-                  </Link>
-                  <p>pembuatan website landing page</p>
-                  <p className="text-muted posted">
-                    Jul, 7 2021 <br />
-                    <i className="fas fa-user-circle"></i> by Donny Marsahid
-                  </p>
-                  <div className="category d-flex">
-                    <div className="category-1">web develop</div>
-                    <div className="category-2">web design</div>
+        <div class="social-media">
+          <Link to="http://instagram.com/donnymrshd" className="icon-sosmed">
+            <i className="fab fa-instagram d-flex align-items-center justify-content-center"></i>
+          </Link>
+          <Link to="http://www.linkedin.com/donny-marsahid" className="icon-sosmed">
+            <i className="fab fa-linkedin d-flex align-items-center justify-content-center"></i>
+          </Link>
+          <Link to="https://github.com/donnymarsahid" className="icon-sosmed">
+            <i className="fab fa-github d-flex align-items-center justify-content-center"></i>
+          </Link>
+          <Link to="https://dribbble.com/donnymrshd" className="icon-sosmed">
+            <i className="fab fa-dribbble d-flex align-items-center justify-content-center"></i>
+          </Link>
+        </div>
+        <div class="container-custom-portfolio">
+          <div className="content mb-5">
+            <div className="container">
+              <div className="box-text mb-4">
+                <h3>Portfolio</h3>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="box-portfolio">
+                    <Link to="/">
+                      <img src={landingPage} className="mb-3" alt="landing-page" />
+                    </Link>
+                    <Link to="/" className="h4">
+                      <h4>Landing Page</h4>
+                    </Link>
+                    <p>pembuatan website landing page</p>
+                    <p className="text-muted posted">
+                      Jul, 7 2021 <br />
+                      <i className="fas fa-user-circle"></i> by Donny Marsahid
+                    </p>
+                    <div className="category d-flex">
+                      <div className="category-1">web develop</div>
+                      <div className="category-2">web design</div>
+                    </div>
+                    <Link to="/">
+                      <button className="btn btn-details mt-2">
+                        <i className="fas fa-info-circle"></i> Detail
+                      </button>
+                    </Link>
                   </div>
-                  <Link to="/">
-                    <button className="btn btn-details mt-2">
-                      <i className="fas fa-info-circle"></i> Detail
-                    </button>
-                  </Link>
+                </div>
+                <div className="col-md-6">
+                  <div className="box-portfolio">
+                    <Link to="/">
+                      <img src={movies} className="mb-3" alt="movies" />
+                    </Link>
+                    <Link to="/" className="h4">
+                      <h4>Movies</h4>
+                    </Link>
+                    <p>pembuatan website Movies</p>
+                    <p className="text-muted posted">
+                      Jul, 7 2021 <br />
+                      <i className="fas fa-user-circle"></i> by Donny Marsahid
+                    </p>
+                    <div className="category d-flex">
+                      <div className="category-1">web develop</div>
+                      <div className="category-2">web design</div>
+                    </div>
+                    <Link to="/">
+                      <button className="btn btn-details mt-2">
+                        <i className="fas fa-info-circle"></i> Detail
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="box-portfolio">
+                    <Link to="/">
+                      <img src={landingPage} className="mb-3" alt="landing-page" />
+                    </Link>
+                    <Link to="/" className="h4">
+                      <h4>Landing Page</h4>
+                    </Link>
+                    <p>pembuatan website landing page</p>
+                    <p className="text-muted posted">
+                      Jul, 7 2021 <br />
+                      <i className="fas fa-user-circle"></i> by Donny Marsahid
+                    </p>
+                    <div className="category d-flex">
+                      <div className="category-1">web develop</div>
+                      <div className="category-2">web design</div>
+                    </div>
+                    <Link to="/">
+                      <button className="btn btn-details mt-2">
+                        <i className="fas fa-info-circle"></i> Detail
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
-              <div className="col-lg-4">
-                <div className="box-portfolio">
-                  <Link to="/">
-                    <img src={movies} className="mb-3" alt="movies" />
-                  </Link>
-                  <Link to="/" className="h4">
-                    <h4>Movies</h4>
-                  </Link>
-                  <p>pembuatan website Movies</p>
-                  <p className="text-muted posted">
-                    Jul, 7 2021 <br />
-                    <i className="fas fa-user-circle"></i> by Donny Marsahid
-                  </p>
-                  <div className="category d-flex">
-                    <div className="category-1">web develop</div>
-                    <div className="category-2">web design</div>
-                  </div>
-                  <Link to="/">
-                    <button className="btn btn-details mt-2">
-                      <i className="fas fa-info-circle"></i> Detail
-                    </button>
-                  </Link>
-                </div>
+              <div className="button text-center mt-5">
+                <Link to="/">
+                  <button className="btn btn-read-more">lihat selengkapnya</button>
+                </Link>
               </div>
-              <div className="col-lg-4">
-                <div className="box-portfolio">
-                  <Link to="/">
-                    <img src={landingPage} className="mb-3" alt="landing-page" />
-                  </Link>
-                  <Link to="/" className="h4">
-                    <h4>Landing Page</h4>
-                  </Link>
-                  <p>pembuatan website landing page</p>
-                  <p className="text-muted posted">
-                    Jul, 7 2021 <br />
-                    <i className="fas fa-user-circle"></i> by Donny Marsahid
-                  </p>
-                  <div className="category d-flex">
-                    <div className="category-1">web develop</div>
-                    <div className="category-2">web design</div>
-                  </div>
-                  <Link to="/">
-                    <button className="btn btn-details mt-2">
-                      <i className="fas fa-info-circle"></i> Detail
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="button text-center mt-5">
-              <Link to="/">
-                <button className="btn btn-read-more">lihat selengkapnya</button>
-              </Link>
             </div>
           </div>
         </div>
+
         <div className="ilustrator container m-auto">
           <div className="jumbotron jumbotron-fluid">
             <div className="container">
